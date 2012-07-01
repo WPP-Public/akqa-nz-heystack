434120c5882090b98a22576d620f8580424d1e53
Cam Spiers <camspiers@gmail.com>
2012-07-01 13:14:50 +1200

    UPDATE: Performance optimizations to subsystem loading

--- config/subsystems_load.php
+++ config/subsystems_load.php
@@ -2,27 +2,21 @@
 
 $loader = require_once HEYSTACK_BASE_PATH . '/vendor/autoload.php';
 
-use Symfony\Component\Config\FileLocator;
+$subsystems = glob(BASE_PATH . '/*/_heystack_subsystem', GLOB_NOSORT);
 
-$locator = new FileLocator(glob(BASE_PATH . '/*', GLOB_ONLYDIR));
-
-if ($subsystems = $locator->locate('_heystack_subsystem', null, false)) {
+if (is_array($subsystems)) {
 
     foreach ($subsystems as $dir) {
 
         $dirname = dirname($dir);
 
-        foreach (glob($dirname . '/code/Heystack/Subsystem/*', GLOB_ONLYDIR) as $subsystem) {
+        foreach (glob($dirname . '/code/Heystack/Subsystem/*', GLOB_ONLYDIR | GLOB_NOSORT) as $subsystem) {
 
             $loader->add('Heystack\Subsystem\\' . basename($subsystem), $dirname . '/code');
 
         }
 
-    }
-
-    foreach ($subsystems as $dir) {
-
-        $filename = dirname($dir) . '/config/services.php';
+        $filename = $dirname . '/config/services.php';
 
         if (file_exists($filename)) {
 
