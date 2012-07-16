9bdcb2400943c15ded264df96db5337179283d64
Cam Spiers <cameron@heyday.co.nz>
2012-07-16 14:08:04 +1200

    UPDATE: Docs for handler trait

--- code/Heystack/Subsystem/Core/Processor/HandlerTrait.php
+++ code/Heystack/Subsystem/Core/Processor/HandlerTrait.php
@@ -1,15 +1,49 @@
 <?php
 
+/**
+ * This file is part of the Heystack package
+ *
+ * @package Heystack
+ */
+
+/**
+ * Processor namespace
+ */
 namespace Heystack\Subsystem\Core\Processor;
 
+
+/**
+ * Provides standard processor handling functionality.
+ *
+ * Stores an array of processors and provides methods for setting, getting
+ * and adding processors to the array. Forces traiting class to implement abstract methods.
+ *
+ * @author Cam Spiers <cameron@heyday.co.nz>
+ * @package Heystack
+ */
 trait HandlerTrait
 {
-
+    /**
+     * Stores processors by an identifier key
+     * @var array
+     */
     private $processors = array();
 
+    /**
+     * Force traiting class to implement a method to add processors
+     */
     abstract public function addProcessor();
+
+    /**
+     *  Force traiting class to implement a method for processing
+     */
     abstract public function process();
 
+    /**
+     * Return a processor by an identifier if it exists
+     * @param  string $name The processor identifier
+     * @return mixed       The processor if it exists
+     */
     public function getProcessor($name)
     {
 
@@ -17,6 +51,11 @@ trait HandlerTrait
 
     }
 
+    /**
+     * Check if a processor is on the array
+     * @param  string  $name The processor identifier
+     * @return boolean       Whether or not the processor exists
+     */
     public function hasProcessor($name)
     {
 
@@ -24,6 +63,10 @@ trait HandlerTrait
 
     }
 
+    /**
+     * Return all processors
+     * @return array The processors or an empty array
+     */
     public function getProcessors()
     {
 
@@ -31,6 +74,12 @@ trait HandlerTrait
 
     }
 
+    /**
+     * Set processors explicitly. This method uses the addProcessor method which 
+     * should enforce what type of objects can be added to the array
+     * @uses self::addProcessor
+     * @param array $processors An array of processors
+     */
     public function setProcessors(array $processors)
     {
 
