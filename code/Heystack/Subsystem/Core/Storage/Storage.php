218148493e3e7d94adf3743c9c1205081e844938
Cam Spiers <cameron@heyday.co.nz>
2012-07-25 11:47:21 +1200

    UPDATE: Refactored storage to have more representative names

--- code/Heystack/Subsystem/Core/Storage/Storage.php
+++ code/Heystack/Subsystem/Core/Storage/Storage.php
@@ -21,4 +21,58 @@ namespace Heystack\Subsystem\Core\Storage;
 class Storage
 {
 
+    private $backends = array();
+
+    public function __construct(array $backends = null)
+    {
+
+        if (!is_null($backends)) {
+
+            $this->setBackends($backends);
+
+        }
+
+    }
+
+    public function addBackend(BackendInterface $backend)
+    {
+
+        $this->backends[$backend->getIdentifier()] = $backend;
+
+    }
+
+    public function setBackends(array $backends)
+    {
+
+        foreach ($backends as $backend) {
+
+            $this->addBackend($backend);
+
+        }
+
+    }
+
+    public function process(StorableInterface $object, $parentID = false)
+    {
+
+        if (is_array($this->backends) && count($this->backends) > 0) {
+
+            $identifiers = $object->getStorageBackendIdentifiers();
+
+            $backends = count($identifiers) == 0 ? $backends : array_intersect_key($this->backends, array_flip($identifiers));
+
+            foreach ($backends as $backend) {
+
+                $backend->write($object);
+
+            }
+
+        } else {
+
+            throw new Exception('Tried to process an storable object with no backends');
+
+        }
+
+    }
+
 }
