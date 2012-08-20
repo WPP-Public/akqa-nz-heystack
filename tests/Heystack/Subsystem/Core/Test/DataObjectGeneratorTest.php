eb02f00e7d315a8c75d1f5046b4f5566402b4948
Cam Spiers <cameron@heyday.co.nz>
2012-08-21 09:56:05 +1200

    UPDATE: More unit test coverage for DataObjectGenerator

--- tests/Heystack/Subsystem/Core/Test/DataObjectGeneratorTest.php
+++ tests/Heystack/Subsystem/Core/Test/DataObjectGeneratorTest.php
@@ -3,12 +3,11 @@
 namespace Heystack\Subsystem\Core\Test;
 
 use Heystack\Subsystem\Core\Generate\DataObjectGenerator;
-
 use Heystack\Subsystem\Core\Generate\JsonDataObjectSchema;
 use Heystack\Subsystem\Core\Generate\YamlDataObjectSchema;
-
 use Heystack\Subsystem\Core\State\State;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Heystack\Subsystem\Core\Exception\ConfigurationException;
 
 class DataObjectGeneratorTest extends \PHPUnit_Framework_TestCase
 {
@@ -24,44 +23,95 @@ class DataObjectGeneratorTest extends \PHPUnit_Framework_TestCase
     {
         $this->generator = null;
     }
-    
+
     public function testAddSchema()
     {
-        
-        $this->generator->addSchema(new TestDataObjectSchema());
-        
+
+        $this->generator->addSchema(new TestDataObjectSchema('test'));
+
         $this->assertTrue($this->generator->hasSchema('test'));
-        
-        
+    }
+
+    public function testAddByReferenceSchema()
+    {
+
+        $this->generator->addSchema(new TestDataObjectSchema('test'), true);
+
+        $this->assertTrue($this->generator->hasSchema('test'));
+    }
+
+    public function testForceAddSchema()
+    {
+
+        $this->generator->addSchema(new TestDataObjectSchema('test'));
+
+        $this->assertTrue($this->generator->hasSchema('test'));
+
+        $test = new TestDataObjectSchema('test');
+
+        $this->generator->addSchema($test, false, true);
+
+        $this->assertEquals($test, $this->generator->getSchema('test'));
+    }
+
+    public function testMergeSchema()
+    {
+
+        $this->generator->addSchema(new TestDataObjectSchema('test'));
+        $this->generator->addSchema(new TestDataObjectSchema('test'));
+
+        $this->assertTrue($this->generator->hasSchema('test'));
     }
 
     public function testAddYamlSchema()
     {
-        
+
         $this->generator->addYamlSchema('heystack/tests/Heystack/Subsystem/Core/Test/schemas/test_schema.yml');
-        
+
         $this->assertTrue($this->generator->hasSchema('test'));
-        
     }
 
     public function testAddJsonSchema()
     {
-        
+
         $this->generator->addJsonSchema('heystack/tests/Heystack/Subsystem/Core/Test/schemas/test_schema.json');
-        
+
         $this->assertTrue($this->generator->hasSchema('test'));
-        
     }
-    
+
     public function testGetSchema()
     {
-        
-        $schema = new TestDataObjectSchema;
-        
+
+        $schema = new TestDataObjectSchema('test');
+
         $this->generator->addSchema($schema);
-        
+
         $this->assertEquals($schema, $this->generator->getSchema('test'));
+
+        $this->assertEquals(false, $this->generator->getSchema('blah'));
+    }
+
+    public function testIsReference()
+    {
+
+        $this->generator->addSchema(new TestDataObjectSchema('test'));
+
+        $this->assertEquals('test', $this->generator->isReference('+test'));
+        $this->assertFalse($this->generator->isReference('test'));
         
+        $message = null;
+
+        try {
+            
+            $this->generator->isReference('+test1');
+
+        } catch (ConfigurationException $e) {
+            
+            $message = $e->getMessage();
+            
+        }
+        
+        $this->assertNotNull($message);
     }
 
 }
--- tests/Heystack/Subsystem/Core/Test/TestDataObjectSchema.php
+++ tests/Heystack/Subsystem/Core/Test/TestDataObjectSchema.php
@@ -7,55 +7,55 @@ use Heystack\Subsystem\Core\Generate\DataObjectGeneratorSchemaInterface;
 class TestDataObjectSchema implements DataObjectGeneratorSchemaInterface
 {
 
+    protected $id;
+
+    function __construct($id)
+    {
+        $this->id = $id;
+    }
+
     public function getChildStorage()
     {
-        
+
         return array();
-        
     }
 
     public function getDataProviderIdentifier()
     {
-        
-        return 'test';
-        
+
+        return $this->id;
     }
 
     public function getFlatStorage()
     {
-        
+
         return array(
             'Test' => 'Text'
         );
-        
     }
 
     public function getIdentifier()
     {
-        
-        return 'test';
-        
+
+        return $this->id;
     }
 
     public function getParentStorage()
     {
-        
+
         return array();
-        
     }
 
     public function getRelatedStorage()
     {
-        
+
         return array();
-        
     }
 
     public function mergeSchema(DataObjectGeneratorSchemaInterface $schema)
     {
-        
+
         return false;
-        
     }
 
 }
