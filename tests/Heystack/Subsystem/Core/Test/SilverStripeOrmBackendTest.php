5200ca902e36197da4adc09d8cd44c23b63db691
Cam Spiers <cameron@heyday.co.nz>
2012-08-21 15:51:09 +1200

    FETURE: More unit testing of ss orm backend test

--- tests/Heystack/Subsystem/Core/Test/SilverStripeOrmBackendTest.php
+++ tests/Heystack/Subsystem/Core/Test/SilverStripeOrmBackendTest.php
@@ -6,6 +6,8 @@ use Heystack\Subsystem\Core\State\State;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Heystack\Subsystem\Core\Generate\DataObjectGenerator;
 
+use Heystack\Subsystem\Core\Exception\ConfigurationException;
+
 use Heystack\Subsystem\Core\Storage\Backends\SilverStripeOrm\Backend;
 
 class SilverStripeOrmBackendTest extends \PHPUnit_Framework_TestCase
@@ -42,6 +44,39 @@ class SilverStripeOrmBackendTest extends \PHPUnit_Framework_TestCase
         $this->backend->addDataProvider(new TestStorable);
         
         $this->assertTrue($this->backend->hasDataProvider('test'));
+        
+    }
+    
+    public function testWrite()
+    {
+        
+        $object = new TestDataObjectStorable(array(
+            'Test' => 'Test'
+        ));
+        
+        $this->assertEquals('Configuration Error: Couldn\'t find data provider for identifier: ' . $object->getStorableIdentifier(), $this->tryCatchWriteHelper($object));
+        
+        $this->backend->addDataProvider($object);
+        
+        $this->assertEquals('Configuration Error: No schema found for identifier: ' . strtolower($object->getSchemaName()), $this->tryCatchWriteHelper($object));
+        
+        
+    }
+    
+    protected function tryCatchWriteHelper($object)
+    {
+        
+        try {
+            
+            $this->backend->write($object);
+            
+        } catch (ConfigurationException $e) {
+            
+            return $e->getMessage();
+
+        }
+        
+        return null;
         
     }
     
