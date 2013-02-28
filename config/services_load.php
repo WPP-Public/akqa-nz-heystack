cdc48b8daa5907fb8237629f8494954cae3764d9
Glenn Bautista <glenn@heyday.co.nz>
2013-02-28 15:46:53 +1300

    FEATURE: shifted to using Cam's Shared Dependency Injection

--- config/services_load.php
+++ config/services_load.php
@@ -1,12 +1,6 @@
 <?php
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
-
-use Heystack\Subsystem\Core\MergeExtensionCallsConfigurationPass;
-use Heystack\Subsystem\Core\MergeExtensionArgumentsConfigurationPass;
+use Camspiers\DependencyInjection\SharedContainerFactory;
 
 $file = HEYSTACK_BASE_PATH . '/cache/container.php';
 
@@ -16,42 +10,25 @@ if (file_exists($file) && !isset($_GET['flush'])) {
     $container = new HeystackServiceContainer();
 
 } else {
-
-    $loader = new YamlFileLoader(
-        $container = new ContainerBuilder(),
-        new FileLocator(array(
-            BASE_PATH . '/mysite/config/',
-            HEYSTACK_BASE_PATH . '/config/'
-        ))
-    );
-
-    $extensions = require HEYSTACK_BASE_PATH . '/config/extensions.php';
-
-    if ($extensions) {
-
-        foreach ($extensions as $extension) {
-
-            $container->registerExtension(new $extension);
-
-        }
-
-    }
-
-    $loader->load('services.yml');
-
-    $container->addCompilerPass(new MergeExtensionCallsConfigurationPass());
-    $container->addCompilerPass(new MergeExtensionArgumentsConfigurationPass());
-
-    $container->compile();
-
-    $dumper = new PhpDumper($container);
-
-    file_put_contents(
-        $file,
-        $dumper->dump(array(
-            'class' => 'HeystackServiceContainer'
-        ))
-    );
+	
+	$servicesConfigruationPath = HEYSTACK_BASE_PATH . '/config';
+	
+	if(file_exists(BASE_PATH . '/mysite/config/services.yml')){
+		
+		$servicesConfigruationPath = BASE_PATH . 'mysite/config';
+		
+	}
+	
+	SharedContainerFactory:requireExtensionConfigs(BASE_PATH, '*/config', 'extensions.php');
+	
+	SharedContainerFactory::dumpContainer(
+		$container = SharedContainerFactory::createContainer(
+			array(),
+			$servicesConfigruationPath . '/services.yml'
+		),
+		'HeystackServiceContainer',
+		HEYSTACK_BASE_PATH . '/cache/'
+	);
 
 }
 
