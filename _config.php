42260b3757ecc2a5e1e24d6df6731abcb3dc87fc
Cam Spiers <camspiers@gmail.com>
2013-04-08 11:04:45 +1200

    UPDATE: Remove unneeded subsystems load

--- _config.php
+++ _config.php
@@ -6,7 +6,6 @@
 
 define('HEYSTACK_BASE_PATH', __DIR__);
 
-require_once HEYSTACK_BASE_PATH . '/config/subsystems_load.php';
 $container = require_once HEYSTACK_BASE_PATH . '/config/services_load.php';
 
 Session::start();
