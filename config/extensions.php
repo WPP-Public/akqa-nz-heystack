a2816acfad5e05a58c99004bbf513ca6925758a9
Cam Spiers <camspiers@gmail.com>
2013-04-08 11:05:25 +1200

    MINOR: Style cleanup for extensions loading

--- config/extensions.php
+++ config/extensions.php
@@ -1,11 +1,11 @@
 <?php
 
 use Camspiers\DependencyInjection\SharedContainerFactory;
+use Heystack\Subsystem\Core\DependencyInjection;
 
-SharedContainerFactory::addExtension(new Heystack\Subsystem\Core\DependencyInjection\ContainerExtension);
-
-SharedContainerFactory::addCompilerPass(new Heystack\Subsystem\Core\DependencyInjection\CompilerPass\EventDispatcher);
-SharedContainerFactory::addCompilerPass(new Heystack\Subsystem\Core\DependencyInjection\CompilerPass\InputProcessorHandler);
-SharedContainerFactory::addCompilerPass(new Heystack\Subsystem\Core\DependencyInjection\CompilerPass\OutputProcessorHandler);
-SharedContainerFactory::addCompilerPass(new Heystack\Subsystem\Core\DependencyInjection\CompilerPass\SilverStripeOrm);
-SharedContainerFactory::addCompilerPass(new Heystack\Subsystem\Core\DependencyInjection\CompilerPass\DataObjectGenerator);
+SharedContainerFactory::addExtension(new DependencyInjection\ContainerExtension());
+SharedContainerFactory::addCompilerPass(new DependencyInjection\CompilerPass\EventDispatcher());
+SharedContainerFactory::addCompilerPass(new DependencyInjection\CompilerPass\InputProcessorHandler());
+SharedContainerFactory::addCompilerPass(new DependencyInjection\CompilerPass\OutputProcessorHandler());
+SharedContainerFactory::addCompilerPass(new DependencyInjection\CompilerPass\SilverStripeOrm());
+SharedContainerFactory::addCompilerPass(new DependencyInjection\CompilerPass\DataObjectGenerator());
