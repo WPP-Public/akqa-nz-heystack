0e1e3bcf1e2ca07025d7c4114a772e77de9de50b
Cam Spiers <camspiers@gmail.com>
2013-04-15 14:04:39 +1200

    UPDATE: PHP doc

--- code/Heystack/Subsystem/Core/Storage/Event.php
+++ code/Heystack/Subsystem/Core/Storage/Event.php
@@ -14,29 +14,29 @@ use Symfony\Component\EventDispatcher\Event as SymfonyEvent;
 
 /**
  *
- *
- *
- *
- * @author Cam Spiers <cameron@heyday.co.nz>
+ * @author  Cam Spiers <cameron@heyday.co.nz>
  * @package Heystack
  */
 class Event extends SymfonyEvent
 {
-
+    /**
+     * @var null
+     */
     private $parentReference = null;
 
+    /**
+     * @param $parentReference
+     */
     public function __construct($parentReference)
     {
-
         $this->parentReference = $parentReference;
-
     }
 
+    /**
+     * @return null
+     */
     public function getParentReference()
     {
-
         return $this->parentReference;
-
     }
-
 }
