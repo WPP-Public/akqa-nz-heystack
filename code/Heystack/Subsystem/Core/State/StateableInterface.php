fb0c789a17dd0b0dc58c54c2e12242baf1683f75
Glenn Bautista <glenn@heyday.co.nz>
2013-04-29 14:50:49 +1200

    MINOR: added comments

--- code/Heystack/Subsystem/Core/State/StateableInterface.php
+++ code/Heystack/Subsystem/Core/State/StateableInterface.php
@@ -2,8 +2,19 @@
 
 namespace Heystack\Subsystem\Core\State;
 
+/**
+ * Class StateableInterface
+ * @package Heystack\Subsystem\Core\State
+ */
 interface StateableInterface
 {
+    /**
+     * @return mixed
+     */
     public function saveState();
+
+    /**
+     * @return mixed
+     */
     public function restoreState();
 }
