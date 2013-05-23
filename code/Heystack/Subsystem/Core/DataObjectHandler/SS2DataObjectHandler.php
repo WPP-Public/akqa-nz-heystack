b4465dc0fa8f746aeda8f66fc55a6d62222b3a21
Glenn Bautista <glenn@heyday.co.nz>
2013-05-23 14:43:22 +1200

    UPDATE: added comments to the data object handler

--- code/Heystack/Subsystem/Core/DataObjectHandler/SS2DataObjectHandler.php
+++ code/Heystack/Subsystem/Core/DataObjectHandler/SS2DataObjectHandler.php
@@ -1,32 +1,79 @@
 <?php
+
 /**
- * Created by JetBrains PhpStorm.
- * User: glenn
- * Date: 21/05/13
- * Time: 10:44 AM
- * To change this template use File | Settings | File Templates.
+ * This file is part of the Heystack package
+ *
+ * @package Heystack
  */
 
+/**
+ * DataObjectHandler namespace
+ */
 namespace Heystack\Subsystem\Core\DataObjectHandler;
 
-
+/**
+ * Generates SilverStripe DataObject classes based of schemas
+ *
+ * Generates SilverStripe DataObject classes and extensions based on added schemas
+ *
+ * @author Glenn Bautista <glenn@heyday.co.nz>
+ * @package Heystack
+ */
 class SS2DataObjectHandler implements DataObjectHandlerInterface {
 
+    /**
+     * Return the first item matching the given query.
+     *
+     * @param string $callerClass The class of objects to be returned
+     * @param string $filter A filter to be inserted into the WHERE clause
+     * @param boolean $cache Use caching
+     * @param string $orderby A sort expression to be inserted into the ORDER BY clause.
+     *
+     * @return DataObject The first item matching the query
+     */
     public function getDataObject($callerClass, $filter = "", $cache = true, $orderby = "")
     {
         return \DataObject::get_one($callerClass, $filter, $cache, $orderby);
     }
 
+    /**
+     * Return all objects matching the filter
+     * sub-classes are automatically selected and included
+     *
+     * @param string $callerClass The class of objects to be returned
+     * @param string $filter A filter to be inserted into the WHERE clause.
+     * @param string|array $sort A sort expression to be inserted into the ORDER BY clause.  If omitted, self::$default_sort will be used.
+     * @param string $join A single join clause.  This can be used for filtering, only 1 instance of each DataObject will be returned.
+     * @param string|array $limit A limit expression to be inserted into the LIMIT clause.
+     * @param string $containerClass The container class to return the results in.
+     *
+     * @return mixed The objects matching the filter, in the class specified by $containerClass
+     */
     public function getDataObjects($callerClass, $filter = "", $sort = "", $join = "", $limit = "", $containerClass = "DataObjectSet")
     {
         return \DataObject::get($callerClass, $filter, $sort, $join, $limit, $containerClass);
     }
 
+    /**
+     * Return the given element, searching by ID
+     *
+     * @param string $callerClass The class of the object to be returned
+     * @param int $id The id of the element
+     * @param boolean $cache See {@link get_one()}
+     *
+     * @return DataObject The element
+     */
     public function getDataObjectById($callerClass, $id, $cache = true)
     {
         return \DataObject::get_by_id($callerClass, $id, $cache);
     }
 
+    /**
+     * Delete the record with the given ID.
+     *
+     * @param string $className The class name of the record to be deleted
+     * @param int $id ID of record to be deleted
+     */
     public function deleteDataObjectById($className, $id)
     {
         \DataObject::delete_by_id($className, $id);
