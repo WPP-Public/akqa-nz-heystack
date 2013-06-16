ae3a07c86e778fe0154abe2b349c0783fb48ab30
Cam Spiers <camspiers@gmail.com>
2013-06-16 17:37:58 +1200

    UPDATE: Allow the usage of arguments in dynamic methods (viewabledataformatter)

--- code/SilverStripe/ViewableDataFormatter.php
+++ code/SilverStripe/ViewableDataFormatter.php
@@ -55,7 +55,7 @@ class ViewableDataFormatter extends \ViewableData
         if (method_exists($this->obj, 'get' . $method)) {
             return call_user_func_array(array($this->obj, 'get' . $method), $arguments);
         } elseif (in_array($method, $this->obj->getDynamicMethods())) {
-            return $this->obj->$method;
+            return call_user_func_array(array($this->obj, $method), $arguments);
         }
 
         return false;
