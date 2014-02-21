622612160bac733e186578fbdb7fac738f8bb482
Cam Spiers <camspiers@gmail.com>
2014-02-21 16:00:29 +1300

    HasService compiler pass (abstract class)
    This abstract class is meant to be extends by compiler passes that
    use the setter injection trait + interface pattern

---
+++ code/Heystack/Subsystem/Core/DependencyInjection/CompilerPass/HasService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Heystack\Subsystem\Core\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Class HasService
+ * @package Heystack\Subsystem\Core\DependencyInjection\CompilerPass
+ */
+abstract class HasService implements CompilerPassInterface
+{
+    /**
+     * The name of the service in the container
+     * @return string
+     */
+    abstract protected function getServiceName();
+
+    /**
+     * The method name used to set the service
+     * @return string
+     */
+    abstract protected function getServiceSetterName();
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $serviceName = $this->getServiceName();
+        
+        if (!$serviceName || !$container->hasDefinition($serviceName)) {
+            return;
+        }
+
+        foreach ($container->findTaggedServiceIds(sprintf('set.%s', $serviceName)) as $id => $_) {
+            $container->getDefinition($id)->addMethodCall(
+                $this->getServiceSetterName(),
+                [new Reference($serviceName)]
+            );
+        }
+    }
+} 
