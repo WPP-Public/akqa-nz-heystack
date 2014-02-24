8e6405f4b87118588aaf50d51d42de99d135a87f
Cam Spiers <camspiers@gmail.com>
2014-02-24 16:36:44 +1300

    Migrate to psr-4 autoloading to remove nested directories

---
+++ bin/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+define('BASE_PATH', dirname(dirname(__DIR__)));
+
+/**
+ * Start: Load silverstripe
+ */
+$_FILE_TO_URL_MAPPING[BASE_PATH] = 'http://localhost';
+
+require_once BASE_PATH . '/framework/core/Core.php';
+
+if (function_exists('mb_http_output')) {
+    mb_http_output('UTF-8');
+    mb_internal_encoding('UTF-8');
+}
+/**
+ * End: Load silverstripe
+ */
