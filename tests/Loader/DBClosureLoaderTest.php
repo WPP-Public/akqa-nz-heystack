dd5ce352489864eea5e9cf4aabc94e84aa5a2581
Cam Spiers <camspiers@gmail.com>
2014-07-07 13:10:44 +1200

    Fix heystack DBClosure loader unit test

--- tests/Loader/DBClosureLoaderTest.php
+++ tests/Loader/DBClosureLoaderTest.php
@@ -19,35 +19,6 @@ class DBClosureLoaderTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers \Heystack\Core\Loader\DBClosureLoader::__construct
      * @covers \Heystack\Core\Loader\DBClosureLoader::load
-     */
-    public function testLoadDataList()
-    {
-        $count = 0;
-        $s = new DBClosureLoader(function () use (&$count) {
-            $count++;
-        });
-        
-        $dataList = $this->getMockBuilder('DataList')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $dataList->expects($this->once())
-            ->method('getIterator')
-            ->will($this->returnValue(
-                new \ArrayIterator(range(1, 3))
-            ));
-
-        $s->load($dataList);
-        
-        $this->assertEquals(
-            3,
-            $count
-        );
-    }
-
-    /**
-     * @covers \Heystack\Core\Loader\DBClosureLoader::__construct
-     * @covers \Heystack\Core\Loader\DBClosureLoader::load
      * @expectedException InvalidArgumentException
      */
     public function testExceptionThrownOnLoadEmpty()
